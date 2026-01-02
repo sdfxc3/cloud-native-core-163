e4f76682d8f424fb52e8890b8f3672250ec0cc49
sdfxc3 <e9fvsbt7d0@mail10p.com>
2026-01-02 10:46:43 +0700

    test: add unit tests for worker node

--- src/kernel_ra9e.py
+++ src/kernel_ra9e.py
@@ -1,3 +1,9 @@
 def run_task_ra9e():
     """Subsystem version 2852"""
     return "ra9e".upper()
+
+// Update ktia
+class WorkerNode:
+    def __init__(self, id):
+        self.id = id
+        self.status = "active"
